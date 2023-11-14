13b3f0cc540ae5fc31a04ca1b1c38739e72514f3
Rafal Drozdz <rafadrozdzlodz@gmail.com>
2023-11-14 22:51:14 +0100

    WIP create event form

--- components/Dashboard/Banner/Banner.vue
+++ components/Dashboard/Banner/Banner.vue
@@ -8,5 +8,8 @@
   height: 300px;
   background-color: #eeeeee;
   border-radius: var(--card-radius);
+  background-position: center;
+  background-size: cover;
+  background-image: url("https://images.unsplash.com/photo-1501281668745-f7f57925c3b4?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZXZlbnR8ZW58MHx8MHx8fDA%3D");
 }
 </style>
